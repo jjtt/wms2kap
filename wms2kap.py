8d802d4e68d55ff1bd9842945293e9fd03ccc373
jjtt <jjtt@mbnet.fi>
2016-09-03 18:30:31 +0300

    reference points to the kap-file

--- wms2kap.py
+++ wms2kap.py
@@ -11,8 +11,8 @@ y1 = 6655590
 x2 = 356670
 y2 = 6665590
 
-(wmsx1, wmsy1) = pyproj.transform(p1,p2,x1,y1)
-(wmsx2, wmsy2) = pyproj.transform(p1,p2,x2,y2)
+(wgsx1, wgsy1) = pyproj.transform(p1,p2,x1,y1)
+(wgsx2, wgsy2) = pyproj.transform(p1,p2,x2,y2)
 
 # calculate image scale
 imagesize = 4096
@@ -24,28 +24,37 @@ mapmetersy = (y2 - y1)
 mapmeters = (mapmetersx + mapmetersy) / 2.0
 scale = int(1.0 / (imagemeters / mapmeters))
 
+averagelatitude = (wgsy1 + wgsy2) / 2.0
 
 kaptmpl = """!kantvik.kap
 VER/2.0
 BSB/NA=kantvik
     NU=1,RA=%d,%d,DU=%d
 KNP/SC=%d,GD=WGS84,PR=MERCATOR
-    PP=60,PI=UNKNOWN,SP=UNKNOWN,SK=0.0,TA=90.0
+    PP=%.2f,PI=UNKNOWN,SP=UNKNOWN,SK=0.0,TA=90.0
     UN=METERS,SD=MLSW,DX=000,DY=000
 CED/SE=1,RE=1,ED=01/01/2016
 OST/1
-REF/1,0,4095,60.00889556,24.24978462
-REF/2,0,0,60.10220688,24.24978462
-REF/3,4095,0,60.10220688,24.42194184
-REF/4,4095,4095,60.00889556,24.42194184
-PLY/1,60.00889556,24.24978462
-PLY/2,60.10220688,24.24978462
-PLY/3,60.10220688,24.42194184
-PLY/4,60.00889556,24.42194184
+REF/1,%d,%d,%.8f,%.8f
+REF/2,%d,%d,%.8f,%.8f
+REF/3,%d,%d,%.8f,%.8f
+REF/4,%d,%d,%.8f,%.8f
+PLY/1,%.8f,%.8f
+PLY/2,%.8f,%.8f
+PLY/3,%.8f,%.8f
+PLY/4,%.8f,%.8f
 DTM/0.00,0.00
 CPH/0.0
 """
 
 wmstmpl = "http://kartta.liikennevirasto.fi/meriliikenne/dgds/wms_ip/merikartta?request=GetMap&BBOX=346670,6655590,356670,6665590&width=4096&height=4096&layers=cells&format=image/png&srs=EPSG:3067"
 
-print(kaptmpl % (imagesize, imagesize, dpi, scale))
+print(kaptmpl % (imagesize, imagesize, dpi, scale, averagelatitude,
+  0,imagesize-1, wgsy1, wgsx1,
+  0,0, wgsy2, wgsx1,
+  0,0, wgsy2, wgsx2,
+  imagesize-1,0, wgsy1, wgsx2,
+  wgsy1, wgsx1,
+  wgsy2, wgsx1,
+  wgsy2, wgsx2,
+  wgsy1, wgsx2))
